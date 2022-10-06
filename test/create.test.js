04575057c88069c396bc115c18258524af1d10b0
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-10-06 17:43:27 +0200

    Import create tests

--- test/create.test.js
+++ test/create.test.js
@@ -40,6 +40,94 @@ test('create', async (t) => {
   await p(ssb.close)(true)
 })
 
+test('create more', (t) => {
+  const server = Server()
+
+  // this is more of an integration test over the api
+  server.tribes2.create({}, (err, data) => {
+    if (err) throw err
+
+    const { groupId, groupKey, groupInitMsg } = data
+    t.true(isGroup(groupId), 'returns group identifier - groupId')
+    t.true(
+      Buffer.isBuffer(groupKey) && groupKey.length === 32,
+      'returns group symmetric key - groupKey'
+    )
+    t.match(
+      groupInitMsg.value.content,
+      /^[a-zA-Z0-9/+]+=*\.box2$/,
+      'encrypted init msg'
+    )
+
+    server.get({ id: groupInitMsg.key, private: true }, (err, value) => {
+      if (err) throw err
+
+      t.deepEqual(
+        value.content,
+        {
+          type: 'group/init',
+          tangles: {
+            group: { root: null, previous: null },
+          },
+        },
+        'can decrypt group/init'
+      )
+
+      // check I published a group/add-member to myself
+      pull(
+        server.createUserStream({
+          id: server.id,
+          private: true,
+          reverse: true,
+        }),
+        pull.map((msg) => msg.value.content),
+        pull.collect((err, msgContents) => {
+          if (err) throw err
+
+          t.deepEqual(
+            msgContents[0], // contents of the latest message
+            {
+              type: 'group/add-member',
+              version: 'v1',
+              groupKey: groupKey.toString('base64'),
+              root: groupInitMsg.key,
+              recps: [groupId, server.id], // me being added to the group
+              tangles: {
+                members: {
+                  root: groupInitMsg.key,
+                  previous: [groupInitMsg.key],
+                },
+                group: {
+                  root: groupInitMsg.key,
+                  previous: [groupInitMsg.key],
+                },
+              },
+            },
+            'The admin was was also added to the group'
+          )
+          server.close()
+          t.end()
+        })
+      )
+    })
+  })
+})
+
+//TODO
+test.skip('tribes.create (opts.addPOBox)', (t) => {
+  const server = Server()
+
+  // this is more of an integration test over the api
+  server.tribes.create({ addPOBox: true }, (err, data) => {
+    if (err) throw err
+
+    t.true(isPoBox(data.poBoxId), 'data.poBoxId')
+
+    server.close()
+    t.end()
+  })
+})
+
 test('get', async (t) => {
   const ssb = Testbot()
 
