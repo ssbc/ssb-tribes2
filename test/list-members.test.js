d4f40593ec2210e6162fae4b0d6dddc179fc68e0
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-10-07 14:10:17 +0200

    Fix listMembers test

--- test/list-members.test.js
+++ test/list-members.test.js
@@ -1,26 +1,30 @@
 const test = require('tape')
 const keys = require('ssb-keys')
-const { Server } = require('../helpers')
+const Testbot = require('./helpers/testbot')
+const pull = require('pull-stream')
 
 test('list members', (t) => {
-  const server = Server()
+  const server = Testbot()
 
-  server.tribes.create(null, (err, { groupId }) => {
+  server.tribes2.create(null, (err, group) => {
     t.error(err, 'created group')
     const newFriends = Array(6)
       .fill(0)
       .map(() => keys.generate().id)
 
-    server.tribes.invite(groupId, newFriends, { text: 'ahoy' }, (err) => {
+    server.tribes2.addMembers(group.id, newFriends, { text: 'ahoy' }, (err) => {
       t.error(err, 'invited friends')
 
-      server.tribes.listAuthors(groupId, (err, authors) => {
-        if (err) throw err
+      pull(
+        server.tribes2.listMembers(group.id),
+        pull.collect((err, authors) => {
+          t.error(err, 'returned members')
 
-        t.deepEqual(authors, [server.id, ...newFriends], 'lists authors')
+          t.deepEqual(authors, [server.id, ...newFriends], 'lists members')
 
-        server.close(t.end)
-      })
+          server.close(true, t.end)
+        })
+      )
     })
   })
 })
