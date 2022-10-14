52f38c5679bff760c2fa1a0083f295f0123f2e62
André Staltz <andre@staltz.com>
2022-10-14 12:26:52 +0300

    improve replicate helper

--- test/helpers/replicate.js
+++ test/helpers/replicate.js
@@ -5,10 +5,21 @@
 const { promisify: p } = require('util')
 
 module.exports = async function replicate(person1, person2) {
+  const clock1 = person1.ebt.getClock();
+  const clock2 = person2.ebt.getClock();
   person1.ebt.request(person1.id, true)
   person2.ebt.request(person2.id, true)
   person1.ebt.request(person2.id, true)
   person2.ebt.request(person1.id, true)
   await p(person1.connect)(person2.getAddress())
-  await new Promise((res) => setTimeout(res, 3000))
+  await new Promise((res) => {
+    const interval = setInterval(() => {
+      const isSynced1 = person1.ebt.getClock()[person2.id] === clock2[person2.id]
+      const isSynced2 = person2.ebt.getClock()[person1.id] === clock1[person1.id]
+      if (isSynced1 && isSynced2) {
+        clearInterval(interval)
+        res()
+      }
+    }, 100)
+  })
 }
