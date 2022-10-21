fd8c1feafdb6a7c8a35305b502d654a60b32be6c
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-10-21 12:51:58 +0200

    Detect too big messages by trying to publish them

---
+++ test/tangle-prune.test.js
@@ -0,0 +1,60 @@
+// SPDX-FileCopyrightText: 2022 Mix Irving
+//
+// SPDX-License-Identifier: LGPL-3.0-only
+
+const test = require('tape')
+//const bipf = require('bipf')
+const Testbot = require('./helpers/testbot')
+const prunePublish = require('../lib/prune-publish')
+
+//const chars = 'abcABC123=+? '.split('')
+//const encodedLength = (obj) => bipf.encodingLength(obj)
+//const randomChar = () => chars.sort(() => (Math.random() < 0.5 ? -1 : +1))[0]
+//const randomText = (length) => {
+//  let output = ''
+//  while (output.length < length) output += randomChar()
+//  return output
+//}
+
+test('prune a message with way too big `previous`', (t) => {
+  const ssb = Testbot()
+  const ssbId = ssb.id
+
+  ssb.tribes2.create(null, (err, group) => {
+    t.error(err, 'no err on group create')
+
+    const msgId = '%RDORgMCjmL6vs51nR4bn0LWNe6wkBfbRJulSdOJsmwg=.sha256'
+    const content = (prevCount, numRecps) => ({
+      type: 'post',
+      text: 'hello!',
+      recps: [group.id, new Array(numRecps - 1).fill(ssbId)],
+      tangles: {
+        group: {
+          root: msgId,
+          previous: new Array(prevCount).fill(msgId),
+        },
+      },
+    })
+
+    //console.time('prune')
+    prunePublish(ssb, content(4000, 16), (err, encMsg16) => {
+      //console.timeEnd('prune')
+      ssb.db.get(encMsg16.key, (err, msg16) => {
+        const msg16len = msg16.content.tangles.group.previous.length
+
+        t.true(
+          msg16len < 4000,
+          `pruned ${4000 - msg16len} from 'previous', ${msg16len} remaining`
+        )
+
+        //const result1 = tanglePrune(content(4000, 1))
+        //t.true(
+        //  encodedLength(result1) <= max1recp,
+        //  `pruned ${4000 - result1.tangles.group.previous.length}`
+        //)
+
+        ssb.close(true, t.end)
+      })
+    })
+  })
+})
