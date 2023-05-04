bea3c9838d49aecfa47bb1f61003e0a391774288
Andre Staltz <andre@staltz.com>
2023-05-04 16:24:59 +0300

    replace group/exclude with group/exclude-member

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const {
     group: {
       addMember: isAddMember,
       content: isContent,
-      exclude: isExclude,
+      excludeMember: isExcludeMember,
       initEpoch: isInitEpoch,
     },
   },
@@ -194,13 +194,13 @@ module.exports = {
         if (err) return cb(clarify(err, "Couldn't get own root when excluding members"))
 
         const excludeContent = {
-          type: 'group/exclude',
+          type: 'group/exclude-member',
           excludes: feedIds,
           recps: [groupId],
         }
         const excludeOpts = {
           tangles: ['members'],
-          isValid: isExclude,
+          isValid: isExcludeMember,
         }
         publish(excludeContent, excludeOpts, (err) => {
           // prettier-ignore
@@ -491,11 +491,11 @@ module.exports = {
         // check if we've been excluded
         pull(
           ssb.db.query(
-            where(and(isDecrypted('box2'), type('group/exclude'))),
+            where(and(isDecrypted('box2'), type('group/exclude-member'))),
             live({ old: true }),
             toPullStream()
           ),
-          pull.filter(isExclude),
+          pull.filter(isExcludeMember),
           pull.filter((msg) =>
             // it's an exclusion of us
             msg.value.content.excludes.includes(myRoot.id)
